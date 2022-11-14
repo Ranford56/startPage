0c517811237f5edfc27842643aeb0d90bc3be899
Ranford56 <rgarciac5@unemi.edu.ec>
2022-11-14 01:07:27 -0500

    simple starpage, using you.com

---
+++ index.js
@@ -0,0 +1,47 @@
+document.onreadystatechange = updateClock()
+let engine_type = localStorage.getItem('engine_type');
+let engine_url = localStorage.getItem('engine_url');
+
+function updateClock() {
+    const date = new Date()
+    document.getElementById("time").innerText = date.toLocaleTimeString([], { hour12: false })
+
+}
+
+setInterval(updateClock, 1000)
+
+
+function Google(){
+    engine_type = 'google';
+    engine_url = 'https://www.google.com/search?q=';
+
+    localStorage.setItem('engine_type', 'google');
+    localStorage.setItem('engine_url', 'https://www.google.com/search?q=');
+}
+
+function SendRequest(){
+    const query = document.getElementById('search').value
+    window.location.href = `https://www.google.com/search?q=${query}`
+}
+
+const searchInput = document.getElementById('search')
+searchInput.addEventListener("keypress", function onEvent(event) {
+    if (event.key === "Enter") {
+        if (engine_url != null) {
+            let request = document.querySelector('.search').value;
+            if (request.includes('>', -1)){
+                Google();
+                if (request.includes('http://', 0) || request.includes('https://', 0 )){
+                    window.location.href = request.substring(1);
+                }
+                else {
+                    window.location.href = "https://" + request.substring(1);
+                }
+                document.getElementById('search').style.animation = 'search-link-redirect 0.5s linear';
+            }
+            else{
+                document.getElementById("search-button").click();
+            }
+        }
+    }
+});
